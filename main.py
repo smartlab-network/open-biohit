128f7b65139602a1ed4cdc2300442772172158c2
niklas.mertsch <niklas.mertsch@stud.uni-goettingen.de>
2022-01-17 13:43:46 +0100

    Update example script

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from biohit_pipettor.pipettor import Pipettor
 
 def run():
     with Pipettor() as inst:
-        print(inst.is_connected)
+        inst.initialize()
+        print(inst.x_position, inst.y_position, inst.z_position)
+        inst.move_z(10)
+        inst.move_xy(30, 30)
+        inst.move_z(0)
+        inst.move_xy(0, 0)
+        print(inst.x_position, inst.y_position, inst.z_position)
 
 
 run()
